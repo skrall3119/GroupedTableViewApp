13cd68e4faff6a2e9fb23b261277c4d7ecb83581
skrall3119 <alexjanci@yahoo.com>
2021-10-27 00:08:08 -0500

    Initial Commit

---
+++ GroupedTableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupedTableViewApp
+//
+//  Created by Alex Janci on 10/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
